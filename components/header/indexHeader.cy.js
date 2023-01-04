cc364e80ce067508c4c44d0ecacdec430a764351
Dillon Smith <dsmith39@gmail.com>
2023-01-04 01:33:16 -0700

    moved header test to cypress folder

--- components/header/indexHeader.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Header from './index'
-
-describe('<Header />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Header />)
-  })
-})
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
+import React from "react";
+import Header from "./index";
+
 describe("Load Index Page", () => {
   it("Visits Index Page", () => {
     cy.visit("http://localhost:3000");
+  });
+});
+
+describe("<Header />", () => {
+  it("renders", () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Header />);
   });
 });
