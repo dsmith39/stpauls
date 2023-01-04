f5e0f43dd74739641e2093fd48b0771b6e633c97
Dillon Smith <dsmith39@gmail.com>
2023-01-03 22:04:10 -0700

    added simple test for index page title

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,30 +1,9 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("has title", async ({ page }) => {
+test("Check For Page Title", async ({ page }) => {
   await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("get started link", async ({ page }) => {
-  await page.goto("/");
-
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
-
-test("test", async ({ page }) => {
-  await page.goto("/");
-  const page1Promise = page.waitForEvent("popup");
-  await page
-    .getByRole("link", {
-      name: "Docs -> Find in-depth information about Next.js features and API.",
-    })
-    .click();
-  const page1 = await page1Promise;
+  await expect(page).toHaveTitle(/St. Paul's Community Church/);
 });
